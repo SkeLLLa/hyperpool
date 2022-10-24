f30822fe3d7489fbca2ec38dfc04b4754c2d7bd9
Commit User <code.commit.user@gmail.com>
2022-10-24 15:58:59 +0200

    test: add stat test for rr

--- src/pools/__tests__/RoundRobin.unit.spec.ts
+++ src/pools/__tests__/RoundRobin.unit.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
+import defer from 'p-defer';
 
 import { PoolItemMock } from '../../__tests__/PoolItemMock.mock';
 import { RoundRobin } from '../RoundRobin';
@@ -56,5 +57,60 @@ describe('Round-robin pool', () => {
         return instance.name;
       }),
     ).resolves.toMatchInlineSnapshot(`"a"`);
+  });
+
+  test('Stats are caluclated', async () => {
+    const workloadA = defer();
+    const workloadB = defer();
+    const workloadC = defer();
+    const workloadD = defer();
+
+    const a = new RoundRobin(new Map([['a', new PoolItemMock('a')]]));
+
+    const task1 = a.execAsync(async () => {
+      return workloadA.promise;
+    });
+    expect(a.stats).toMatchInlineSnapshot(`
+      {
+        "free": Infinity,
+        "queued": 0,
+        "running": 1,
+        "size": 1,
+      }
+    `);
+    const task2 = a.execAsync(async () => {
+      return workloadB.promise;
+    });
+    const task3 = a.execAsync(async () => {
+      return workloadC.promise;
+    });
+    expect(a.stats).toMatchInlineSnapshot(`
+      {
+        "free": Infinity,
+        "queued": 0,
+        "running": 3,
+        "size": 1,
+      }
+    `);
+    workloadB.resolve('b:done');
+    await expect(task2).resolves.toMatchInlineSnapshot(`"b:done"`);
+    expect(a.stats).toMatchInlineSnapshot(`
+      {
+        "free": Infinity,
+        "queued": 0,
+        "running": 2,
+        "size": 1,
+      }
+    `);
+    const task4 = a.execAsync(async () => {
+      return workloadD.promise;
+    });
+    workloadA.resolve('a:done');
+    workloadC.resolve('c:done');
+    workloadD.resolve('d:done');
+
+    await expect(task1).resolves.toMatchInlineSnapshot(`"a:done"`);
+    await expect(task3).resolves.toMatchInlineSnapshot(`"c:done"`);
+    await expect(task4).resolves.toMatchInlineSnapshot(`"d:done"`);
   });
 });
